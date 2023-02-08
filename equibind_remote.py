766be25213356aee88a2b6f14671a351d5658c35
scottmreed <53488644+scottmreed@users.noreply.github.com>
2023-02-08 12:31:01 -0700

    Updates remote equibind on CPU PG server

--- equibind_remote.py
+++ equibind_remote.py
@@ -5,35 +5,34 @@ import os
 # Caution: anything sent through run_command will excute on the server you are connected to as written.
 # Look closely at these commands before executing them remotely.
 
+import os
+from dotenv import load_dotenv, find_dotenv
+from server_connections import Pharmacogenomics
+import re
+import json
+
+# Caution: anything sent through run_command will execute on the server you are connected to, as written.
+# Look closely at these commands before executing them remotely.
 load_dotenv(find_dotenv())
 
-username = os.getenv('Kenari_USER')
-password = os.getenv('Kenari_PASSWORD')
+username = os.getenv('pharmaco_server_USER')
+password = os.getenv('pharmaco_server_PASSWORD')
 
-equibind_server = Kenari(username=username, password=password)
+if __name__ == '__main__':
 
-batch_file = """singularity exec ../../singularity/equibind7_latest.sif conda run -n equibind python \
-/EquiBind/multiligand_inference.py --config=/EquiBind/configs_clean/inference.yml \
--o 'output' -r name1_protein.pdb -l ligands.sdf >& gpucode.log
-"""
+    alpha_folder = os.path.join('/', 'home', 'boss', 'website_activity', 'tmp')
+    equibind_server = Pharmacogenomics(username=username, password=password)
+    protein_folders, success = equibind_server.run_command(f'ls {alpha_folder}')
+    protein_list = protein_folders.split("\n")
+    for protein in protein_list[0]:
 
-equibind_server.send_batch('equibind_batch.sh', batch_file)
-equibind_server.send_chmod('equibind_batch.sh')
-output, _ = equibind_server.run_command('./equibind_batch.sh')
-command_input = 'cat gpucode.log'
-output, _ = equibind_server.run_command(command_input)
-print(output)
-
-# username = os.getenv('ALDERAAN_USER')
-# password = os.getenv('ALDERAAN_PASSWORD')
-# equibind_server = Alderaan(username=username, password=password)
-# equibind_pharmaco_folder = os.path.join('/', 'home', 'reedsc', 'temp_file')
-# sif_folder = os.path.join('/', 'home', 'reedsc', 'Equibind')
-# equibind_folder = os.path.join('/', 'home', 'reedsc', 'Equibind', 'EquiBind')
-# kenari_sif_folder = os.path.join('/', 'singularity')
-# inference_folder = os.path.join('/', 'opt', 'EquiBind', 'EquiBind')
-#
-
-# command_input = f'singularity exec {kenari_sif_folder}/equibind4_latest.sif python /opt/EquiBind/EquiBind/inference.py --config={inference_folder}/configs_clean/inference.yml'
-# output, _ = equibind_server.run_command(command_input)
-# print(output)
+        batch_file = """
+        ls -l 
+        pwd >& test.log
+        """
+        equibind_server.send_batch('equibind_batch.sh', batch_file)
+        equibind_server.send_chmod('equibind_batch.sh')
+        output, _ = equibind_server.run_command('./equibind_batch.sh')
+        command_input = 'cat test.log'
+        output, _ = equibind_server.run_command(command_input)
+        print(output)
